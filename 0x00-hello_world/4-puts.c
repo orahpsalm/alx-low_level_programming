4d29a47dc93f8c800f55b7c9244cf48b8984537d
orahpsalm <psalmorahcle@gmail.com>
2022-03-10 20:59:12 -0800

    Correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main -Entry point
+ *
+ * Description: prints "with proper grammar, but the outcome is a piece of
+ * art,"
+ *
+ * Return: Always 0 (Success)
+ * */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
