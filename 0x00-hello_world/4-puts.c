2a3f3c97a3292a1322d823234a5f23e4bd0e30d3
orahpsalm <psalmorahcle@gmail.com>
2022-03-11 04:08:32 -0800

    Modify 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 
 /*
- * main - Entry point
- *
- * Description: prints "with proper grammar, but the outcome is a piece of
+ * main - prints "with proper grammar, but the outcome is a piece of
  * art,"
  *
  * Return: Always 0 (Success)
