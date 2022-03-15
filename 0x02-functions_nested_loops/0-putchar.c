1afecf149aeeb968dddfd797b87b7d73648fdb82
orahpsalm <psalmorahcle@gmail.com>
2022-03-15 12:08:38 -0700

    Add programme that prints _putchar and then new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/*
+ * main - Entry point
+ * Description: Prints _putchar and then new line
+ * Return: Always returns (0)
+ */
+int main(void)
+{
+	_putchar("_putchar\n");
+	return (0);
+}
