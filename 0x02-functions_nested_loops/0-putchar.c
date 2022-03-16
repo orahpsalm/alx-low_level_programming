6a4b4042deb28b2ba09d73888d6ee298d3e408c2
orahpsalm <psalmorahcle@gmail.com>
2022-03-16 12:18:26 -0700

    Compile 0-putchar.c to 0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,20 @@
 #include "main.h"
 
-/*
- * main - Entry point
+/**
+ * main - check description
  * Description: Prints _putchar and then new line
  * Return: Always returns (0)
  */
 int main(void)
 {
-	_putchar("_putchar\n");
+	char *word = "_putchar";
+
+	while (*word != '\0')
+	{
+		putchar(*word);
+		word++;
+	}
+	putchar('\n');
+
 	return (0);
 }
