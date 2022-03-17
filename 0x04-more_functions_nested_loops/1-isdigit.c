f02d755196bab4eb4b00c799fd47320f00f292d3
orahpsalm <psalmorahcle@gmail.com>
2022-03-17 19:47:54 +0000

    (3)Add C sourcecode 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -6,10 +6,10 @@
  *_isdigit - checks for digit 0 through 9
  *
  * Return: 1 if ci is digit, 0 otherwise
- x*/
+ */
 int _isdigit(int c)
 {
-	if (c >= "0" && c <= "9")
+	if (c >= '0' && c <= '9')
 		return (1);
 	else
 		return (0);
