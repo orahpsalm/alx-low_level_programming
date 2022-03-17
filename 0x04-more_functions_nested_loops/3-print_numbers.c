cc299642751716cdba31a75be6d6d6958643d00e
orahpsalm <psalmorahcle@gmail.com>
2022-03-17 20:06:00 +0000

    Add sourcecode 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ *  Return: void
+ */
+void print_numbers(void)
+{
+	int c;
+	for (c = '0'; c <= '9'; c++)
+		_putchar(c);
+	-putchar('\n');
+}
