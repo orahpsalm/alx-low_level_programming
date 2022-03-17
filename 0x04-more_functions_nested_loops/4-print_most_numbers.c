9c132f4502a5c4d5c326194f2bc9cfa181cdccc1
orahpsalm <psalmorahcle@gmail.com>
2022-03-17 20:18:33 +0000

    Add source code 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints from 0-9 excluding 2 and 4
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int c;
+	for (c = '0'; c <= '9'; c++)
+	{
+		if (c != '2' && c != '4')
+			_putchar(c);
+	}	
+	_putchar('\n');
+}
