c6a05ff82025323d324ece7635c9bb9663a97934
orahpsalm <psalmorahcle@gmail.com>
2022-03-17 19:41:53 +0000

    Add C sourcecode 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * @c: character to be checked
+ *
+ *_isdigit - checks for digit 0 through 9
+ *
+ * Return: 1 if ci is digit, 0 otherwise
+ x*/
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
+}
