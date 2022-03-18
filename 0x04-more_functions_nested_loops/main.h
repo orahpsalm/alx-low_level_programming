0e3935719f7ece857cdb916eca8ea94d1882aca8
orahpsalm <psalmorahcle@gmail.com>
2022-03-18 14:32:19 +0000

    Modify main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef INV_MAIN_H
-#define INV_MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 int _isupper(int c);
@@ -11,5 +11,7 @@ void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
 
 #endif
