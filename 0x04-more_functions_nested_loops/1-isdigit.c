1a1d24dd849fc0b362a569c914bada52115a7389
orahpsalm <psalmorahcle@gmail.com>
2022-03-18 14:51:42 +0000

    Conform source-code in 1-isdigit.c to betty

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,16 +1,13 @@
 #include "main.h"
-
 /**
- * @c: character to be checked
- *
- * _isdigit - checks for digit 0 through 9
- *
- * Return: 1 if c is digit, 0 otherwise
- */
+*_isdigit - Check for digits (0 through 9)
+*@c: int type param
+*Return: 1 if digit, else 0
+*/
 int _isdigit(int c)
 {
-	if (c >= '0' && c <= '9')
-		return (1);
-	else
-		return (0);
+	if (c > 47 && c < 58)
+	return (1);
+
+	return (0);
 }
