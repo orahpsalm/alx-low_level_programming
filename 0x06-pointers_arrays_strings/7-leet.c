6ab1252dbcd6e7b1b681ecf7bdcbb46260c9700d
orahpsalm <psalmorahcle@gmail.com>
2022-03-24 15:35:55 +0000

    Edit file 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -5,7 +5,7 @@
  * @s: string array
  * Return: string array
  */
-*leet(char *s)
+char *leet(char *s)
 {
 	int i;
 
@@ -14,13 +14,13 @@
 		if (s[i] == 'a' || s[i] == 'A')
 			s[i] = '4';
 		if (s[i] == 'e' || s[i] == 'E')
-                        s[i] = '3';
+			s[i] = '3';
 		if (s[i] == 'o' || s[i] == 'O')
-                        s[i] = '0';
+			s[i] = '0';
 		if (s[i] == 't' || s[i] == 'T')
-                        s[i] = '7';
+			s[i] = '7';
 		if (s[i] == 'l' || s[i] == 'L')
-                        s[i] = '1';
+			s[i] = '1';
 	return (s);
 	}
 }
