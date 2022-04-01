6f296ba8445da5e775c71d38542900c380a897c8
orahpsalm <psalmorahcle@gmail.com>
2022-04-01 20:52:05 +0000

    Add file 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * @argc: Counts number of parameters that go into main
+ * @argv: Pointer of array of pointers containing strings entering main
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[argc -1]);
+	return (0);
+}
