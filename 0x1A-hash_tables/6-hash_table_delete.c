14f975a71c8668a70a03b7f91a11badd0e5ed4b8
orahpsalm <psalmorahcle@gmail.com>
2022-07-01 20:35:10 +0100

    Update 6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,44 +1,35 @@
 #include "hash_tables.h"
+
 /**
- * hash_table_delete - function that deletes a hash table.
- *
- * @ht: is the hash table
+ * hash_table_delete - deletes hash table
+ * @ht: hash table
  */
+
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_node_t *head = NULL;
-	unsigned long int i = 0;
+	unsigned long int index;
+	hash_node_t *node, *next_node;
 
-	/*Checking if ht exist*/
 	if (ht == NULL)
-	{
 		return;
-	}
-	while (i < ht->size)
+
+	index = 0;
+	while (index < ht->size)
 	{
-		head = ht->array[i];
-		if (head != NULL)
+		node = ht->array[index];
+		while (node != NULL)
 		{
-			recursive_delete(head);
+			next_node = node->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+			if (next_node != NULL)
+				node = next_node;
+			else
+				break;
 		}
-		i++;
+		index++;
 	}
 	free(ht->array);
 	free(ht);
-}
-/**
- * recursive_delete - function that use the recursive method
- * to delete the liked list recursively
- *
- * @node: is a node of hash table
- */
-void recursive_delete(hash_node_t *node)
-{
-	if (node->next)
-	{
-		recursive_delete(node->next);
-	}
-	free(node->key);
-	free(node->value);
-	free(node);
 }
